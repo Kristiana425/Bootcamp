c4bb46481305ab308b116cf5e7c8ba94e4b4a39a
User <User@DESKTOP-O51R4A1>
2023-05-03 10:42:01 +0200

    Add project files.

---
+++ Bootcamp/Program.cs
@@ -0,0 +1,34 @@
+using Bootcamp.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Connection String
+string dbConnectionString = "Data Source = DESKTOP-O51R4A1\\SQLEXPRESS; Initial Catalog = AuthorBook; Integrated Security = True; Pooling = False;Encrypt=false;TrustServerCertificate=true";
+
+//Configure AppDbContext
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(dbConnectionString));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
